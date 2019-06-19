fe75b9314d5b2592fdd928f6e0fa6b5a7f016897
Michael Williams <manicdesigner@gmail.com>
2019-06-19 19:21:28 -0400

    feat(Movie.js) + id from this.props.match.params.id dynamic included

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -10,8 +10,9 @@ export default class Movie extends Component {
   }
 
   componentDidMount() {
+    console.log('id IN CDM', this.props.match.params);
     // change this line to grab the id passed on the URL
-    const id = 4;
+    const { id } = this.props.match.params;
     this.fetchMovie(id);
   }
 
