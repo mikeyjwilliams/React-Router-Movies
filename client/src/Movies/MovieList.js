4d29fb84021efcddc601fa45d458c7d669018853
Michael Williams <manicdesigner@gmail.com>
2019-06-19 14:55:07 -0400

    feat(MovieList.js) + Link import + Link wrap around MovieDetails component to make them links

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 export default class MovieList extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      movies: []
+      movies: [],
     };
   }
 
@@ -22,9 +23,11 @@ export default class MovieList extends Component {
 
   render() {
     return (
-      <div className="movie-list">
+      <div className='movie-list'>
         {this.state.movies.map(movie => (
-          <MovieDetails key={movie.id} movie={movie} />
+          <Link to={`/movies/${movie.id}`} key={movie.id}>
+            <MovieDetails key={movie.id} movie={movie} />
+          </Link>
         ))}
       </div>
     );
@@ -34,18 +37,18 @@ export default class MovieList extends Component {
 function MovieDetails({ movie }) {
   const { title, director, metascore, stars } = movie;
   return (
-    <div className="movie-card">
+    <div className='movie-card'>
       <h2>{title}</h2>
-      <div className="movie-director">
+      <div className='movie-director'>
         Director: <em>{director}</em>
       </div>
-      <div className="movie-metascore">
+      <div className='movie-metascore'>
         Metascore: <strong>{metascore}</strong>
       </div>
       <h3>Actors</h3>
 
       {stars.map(star => (
-        <div key={star} className="movie-star">
+        <div key={star} className='movie-star'>
           {star}
         </div>
       ))}
