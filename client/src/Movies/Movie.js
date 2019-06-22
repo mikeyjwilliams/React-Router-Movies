dc61f4b4046c8dc43a55cb2b7530093a562f96e4
Michael Williams <manicdesigner@gmail.com>
2019-06-22 13:46:12 -0400

    feat(Movies.js) + click handler on save-button

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -26,27 +26,28 @@ export default class Movie extends Component {
       });
   };
   // Uncomment this code when you're ready for the stretch problems
-  // componentWillReceiveProps(newProps){
-  //   if(this.props.match.params.id !== newProps.match.params.id){
-  //     this.fetchMovie(newProps.match.params.id);
-  //   }
-  // }
+  componentWillReceiveProps(newProps) {
+    if (this.props.match.params.id !== newProps.match.params.id) {
+      this.fetchMovie(newProps.match.params.id);
+    }
+  }
 
-  // saveMovie = () => {
-  //   const addToSavedList = this.props.addToSavedList;
-  //   addToSavedList(this.state.movie)
-  // }
+  saveMovie = () => {
+    const addToSavedList = this.props.addToSavedList;
+    addToSavedList(this.state.movie);
+  };
 
   render() {
     if (!this.state.movie) {
       return <div>Loading movie information...</div>;
     }
 
-    const { title, director, metascore, stars } = this.state.movie;
     return (
       <div className='save-wrapper'>
         <MovieCard {...this.state.movie} />
-        <div className='save-button'>Save</div>
+        <div onClick={this.saveMovie} className='save-button'>
+          Save
+        </div>
       </div>
     );
   }
